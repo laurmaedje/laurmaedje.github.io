c0d3942242ce1b1d945f394024bdb4b673c127d1
Laurenz <laurmaedje@gmail.com>
2022-08-15 21:56:20 +0200

    Add Ahrefs verification file

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ const moment = require("moment");
 module.exports = function (config) {
   config.addPassthroughCopy("assets");
   config.addPassthroughCopy("styles.css");
+  config.addPassthroughCopy(
+    "ahrefs_ab196c32b430cd534174470f3bfc67da55eb94fc3c0b88a09f58dc62f75ec411"
+  );
   config.addPlugin(pluginSyntaxHighlight);
   config.addFilter("dateIso", (date) => moment(date).toISOString());
   config.addFilter("dateReadable", (date) => moment(date).utc().format("LL"));
